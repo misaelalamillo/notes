27a1dc28631833d68f6c89d324fe567665a5b519
misaelalamillo <misaelalamillo@gmail.com>
2020-03-06 23:38:24 -0600

    final commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var path = require("path");
+var fs = require("fs")
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -10,6 +11,41 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
+var notes = [];
+  
+  
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"));
   });
+  
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+  
+  app.get("/api/notes", function(req, res) {
+    return res.json(notes);
+  });
+  
+  app.delete("/api/notes/:id", function(req, res) {
+    var chosen = req.params.note;
+  
+    console.log(chosen);
+  
+    for (var i = 0; i < notes.length; i++) {
+      if (chosen === notes[i].routeName) {
+        return res.json(notes[i]);
+      }
+    }
+  
+    return res.json(false);
+  });
+
+  app.post("/api/notes", function(req, res) {
+   notes.push(req,body);
+  
+  });
+  
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
